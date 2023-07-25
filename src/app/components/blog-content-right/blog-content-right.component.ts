74ce73f88b5f44aaac8decff4016a8da47974587
Omar Guerra Alfaro <102171656+omGuerraAlfaro@users.noreply.github.com>
2023-07-25 11:42:07 -0400

    ...

--- src/app/components/blog-content-right/blog-content-right.component.ts
+++ src/app/components/blog-content-right/blog-content-right.component.ts
@@ -7,7 +7,7 @@ declare const Liferay: any;
   styleUrls: ['./blog-content-right.component.css']
 })
 export class BlogContentRightComponent implements OnInit {
-  private readonly LIFERAY_API = 'http://192.168.1.32:8080/o/c/presses/';
+  private readonly LIFERAY_API = '/o/c/presses/';
   token?: string;
   contentMostView: any[] = [];
   contentFeature: any[] = [];
